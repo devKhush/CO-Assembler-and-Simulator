7591a91062b5d2d6e1684be985c1d6dd1b8d0613
devKhush <khushdev20211@iiitd.ac.in>
2021-08-10 22:42:37 +0530

    add

---
+++ Simple-Assembler/instruction_table.py
@@ -0,0 +1,7 @@
+# instructions, opcodes, operands, type 
+
+instructions = ["add","sub","mov","mov","ld","st","mul","div","rs","ls","xor","or","and","not","cmp","jmp","jlt","jgt","je","hlt"]
+opcode = ["00000","00001","00010","00011","00100","00101","00110","00111","01000","01001","01010","01011","01100","01101","01110","01111","10000"]
+type = ["A","A","B","C","D","D","A","C","B","B","A","A","A","C","C","E","E","E","E","F"]
+operands = [3,3,2,2,2,2,3,2]
+# edit
