4a31c164d3f8f12b90ad214e6d862a3b6029b1c4
devKhush <khushdev20211@iiitd.ac.in>
2021-08-12 23:10:41 +0530

    instruction

--- Simple-Assembler/main.py
+++ Simple-Assembler/main.py
@@ -1,11 +1,10 @@
 from sys import stdin
 from registers import *
-import functions
 
-def identify_function(input):
-    functions.addition(reg0,reg1,reg2)
-    
+program_counter = 0
 
 for line in stdin:
     if line == '':
-        break
+        continue
+
+    instruction = line.split()
