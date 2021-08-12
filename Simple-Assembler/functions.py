5bcae3a0ece46e1d9a0d55825d178f8195a675aa
devKhush <khushdev20211@iiitd.ac.in>
2021-08-12 22:29:32 +0530

    commit

--- Simple-Assembler/functions.py
+++
@@ -1,7 +0,0 @@
-import registers
-
-def addition(reg1,reg2,reg3):
-    reg3 = reg1 +reg2
-    if reg3>255:
-        registers.V=1
-
--- Simple-Assembler/instruction_table.py
+++ Simple-Assembler/instruction_table.py
@@ -1,8 +1,18 @@
 # instructions, opcodes, operands, type 
+import registers
+
 instructions = ["add","sub","mov","mov","ld","st","mul","div","rs","ls","xor","or","and","not","cmp","jmp","jlt","jgt","je","hlt"]
 opcode = ["00000","00001","00010","00011","00100","00101","00110","00111","01000","01001","01010","01011","01100","01101","01110","01111","10000"]
 type = ["A","A","B","C","D","D","A","C","B","B","A","A","A","C","C","E","E","E","E","F"]
-operands = [3,3,2,2,2,2,3,2]
+operands = [3,3,2,2,2,2,3,2,2,2,3,3,3,2,2,1,1,1,1,0]
+
+
+
+def addition(reg1,reg2,reg3):
+    reg3 = reg1 +reg2
+    if reg3>255:
+        registers.V=1
+
 
 
 
