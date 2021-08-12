b8e710decdcd89b4cdb4f9f7139b6212990b8493
devKhush <khushdev20211@iiitd.ac.in>
2021-08-12 23:11:25 +0530

    instructions

--- Simple-Assembler/instruction_table.py
+++ Simple-Assembler/instruction_table.py
@@ -6,12 +6,26 @@ opcode = ["00000","00001","00010","00011","00100","00101","00110","00111","01000
 type = ["A","A","B","C","D","D","A","C","B","B","A","A","A","C","C","E","E","E","E","F"]
 operands = [3,3,2,2,2,2,3,2,2,2,3,3,3,2,2,1,1,1,1,0]
 
+type_A_instructions = ['add','sub','mul','xor','or','and']
+type_B_instructions = ['mov','rs','ls','rs','ls']
+type_C_instructions = ['mov','div','not','cmp']
+type_D_instructions = ['ld','st']
+type_E_instructions = ['jmp','jlt','jgt','je']
+type_F_instructions = ['hlt']
 
 
 def addition(reg1,reg2,reg3):
-    reg3 = reg1 +reg2
-    if reg3>255:
+    reg1 = reg2 +reg3
+    if reg1>255:
         registers.V=1
+
+
+def sub(reg1,reg2,reg3):
+    reg1 = reg2 - reg3
+    if reg1<0:
+        reg1 =0 
+        registers.V=1
+
 
 
 
