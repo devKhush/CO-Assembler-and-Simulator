f5a76a44c91e2d996801f9e00306a469fbf32287
devKhush <khushdev20211@iiitd.ac.in>
2021-08-12 15:00:02 +0530

    files added

---
+++ Simple-Assembler/main.py
@@ -0,0 +1,10 @@
+from sys import stdin
+import functions
+
+def identify_function(input):
+    
+
+for line in stdin:
+    if line == '': # If empty string is read then stop the loop
+        break
+    identify_function(line) 
