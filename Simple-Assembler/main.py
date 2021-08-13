b96ef2b236abe0aece780207d9c2a7813c84de91
devKhush <khushdev20211@iiitd.ac.in>
2021-08-13 23:48:28 +0530

    working on main

--- Simple-Assembler/main.py
+++ Simple-Assembler/main.py
@@ -1,6 +1,7 @@
 from sys import stdin
 from registers import *
 
+variables_encountered_again = 0
 program_counter = 0
 halt_found = 0
 all_varaibles_defined =[]
@@ -12,4 +13,8 @@ for line in stdin:
     instruction_entered = line.split()
 
     if halt_found==1:
-        print(f"'Syntax Error' In line no.{program_counter}: 'hlt' not being used as last statement")
+        print(f"'Syntax Error' In line no. {program_counter}: 'hlt' not being used as last statement")
+        break
+    
+    if variables_encountered_again==1 and instruction_entered[0]=='var':
+        print(f"'Syntax Error' In line no. {program_counter}: Variables NOT defined in the beginning")
