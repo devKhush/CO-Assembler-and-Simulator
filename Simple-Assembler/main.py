6f97c8c0a5cf5d7833cd138b4e7e4bda1f6f49ea
devKhush <khushdev20211@iiitd.ac.in>
2021-08-13 23:17:28 +0530

    i am working on main

--- Simple-Assembler/main.py
+++ Simple-Assembler/main.py
@@ -2,9 +2,14 @@ from sys import stdin
 from registers import *
 
 program_counter = 0
+halt_found = 0
+all_varaibles_defined =[]
 
 for line in stdin:
     if line == '':
         continue
 
-    instruction = line.split()
+    instruction_entered = line.split()
+
+    if halt_found==1:
+        print(f"'Syntax Error' In line no.{program_counter}: 'hlt' not being used as last statement")
