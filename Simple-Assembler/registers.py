115ce9d1f2fdf2390811905a139f6ff704a950e0
sumitiiitd <sumit20250@iiitd.ac.in>
2021-08-14 16:14:08 +0530

    Merge branch 'master' of https://github.com/devKhush/CO_Assignment

--- Simple-Assembler/registers.py
+++ Simple-Assembler/registers.py
@@ -11,3 +11,4 @@ G = 0
 E = 0
 flags = {'FLAGS': '111'}
 binary_of_registers = {'R0': '000', 'R1': '001', 'R2': '010', 'R3': '011', 'R4': '100', 'R5': '101', 'R6': '110'}
+def typeA_fun(instruction)
