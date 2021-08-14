33531ebb2c80d41cac704aa0e19e5925fc69495d
devKhush <khushdev20211@iiitd.ac.in>
2021-08-14 23:02:55 +0530

    do said changes in type A

--- Simple-Assembler/registers.py
+++ Simple-Assembler/registers.py
@@ -18,14 +18,14 @@ def typeA_fun(instruction_entered) :
     ml= op_code+"00"+r1+r2+r3 #converted into machine code 
     #add function
     if (ml[:5]=="00000"):
-        binary_of_registers[r1][1] = binary_of_registers[r2][1]+binary_of_registers[r3][1]
+        binary_of_registers[instruction_entered[1]][1] = binary_of_registers[instruction_entered[2]][1]+binary_of_registers[instruction_entered[3]][1]
         if binary_of_registers[r1][1]>255: #overflow:when the answer is more than 255
             V=1
-            binary_of_registers[r1][1]=0
+            binary_of_registers[r1][1]=0    # remove this and set lower 16 bits values into Reg1
 
     #subtraction function
     elif (ml[:5]=="00001"):
-        binary_of_registers[r1][1]=binary_of_registers[r2][1]-binary_of_registers[r3][1]
+        binary_of_registers[instruction_entered[1]][1]=binary_of_registers[instruction_entered[2]][1]-binary_of_registers[instruction_entered[3]][1]
         if binary_of_registers[r1][1]<0: #overflow : when the sub is less than 0
             V=1
             binary_of_registers[r1][1]=0
@@ -33,20 +33,20 @@ def typeA_fun(instruction_entered) :
 
     #multiplication function
     elif (ml[:5]=="00010"):
-        binary_of_registers[r1][1]=binary_of_registers[r2][1]*binary_of_registers[r3][1]
-        if binary_of_registers[r1][1]>255: #overflow:when the answer is more than 255
+        binary_of_registers[instruction_entered[1]][1]=binary_of_registers[instruction_entered[2]][1]*binary_of_registers[instruction_entered[3]][1]
+        if binary_of_registers[instruction_entered[1]][1]>255: #overflow:when the answer is more than 255
             V=1
-            binary_of_registers[r1][1]=0
+            binary_of_registers[r1][1]=0  # remove this and set lower 16 bits values into Reg1
 
     #bitwise XOR
     elif(ml[:5]=="01010"):
-        binary_of_registers[r1][1]=binary_of_registers[r2][1]^binary_of_registers[r3][1]
+        binary_of_registers[instruction_entered[1]][1]=binary_of_registers[instruction_entered[2]][1]^binary_of_registers[instruction_entered[3]][1]
 
     #bitwise OR
     elif(ml[:5]=="01011"):
-        binary_of_registers[r1][1]=binary_of_registers[r2][1] | binary_of_registers[r3][1]
+        binary_of_registers[instruction_entered[1]][1]=binary_of_registers[instruction_entered[2]][1] | binary_of_registers[instruction_entered[3]][1]
 
     #bitwise AND
     elif (ml[:5]=="01100"):
-        binary_of_registers[r1][1]=binary_of_registers[r2][1] & binary_of_registers[r3][1]
+        binary_of_registers[instruction_entered[1]][1]=binary_of_registers[instruction_entered[2]][1] & binary_of_registers[instruction_entered[3]][1]
         print(ml)
