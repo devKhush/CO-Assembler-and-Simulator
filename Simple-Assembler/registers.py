7fc72efe62ad90c178cfca657ee9b1d95a7da7a8
devKhush <khushdev20211@iiitd.ac.in>
2021-08-14 22:58:17 +0530

    changes in reg

--- Simple-Assembler/registers.py
+++ Simple-Assembler/registers.py
@@ -1,22 +1,24 @@
-from main import *
-from instructions import *
+import main
+import instructions
+
 V = 0
 L = 0
 G = 0
 E = 0
 flags = {'FLAGS': '111'}
 binary_of_registers = {'R0': ['000',0], 'R1':['001',0],'R2': ['010',0], 'R3':['011',0], 'R4': ['100',0], 'R5':['101',0], 'R6':['110',0]}
+
 def typeA_fun(instruction_entered) :
-    op_code = type_A_instructions[instruction_entered[0]]
+    op_code = instructions.type_A_instructions[instruction_entered[0]]
 
     #taking register's binary value from the dictionaries
     r1= binary_of_registers[instruction_entered[1]][0]
     r2= binary_of_registers[instruction_entered[2]][0]
     r3= binary_of_registers[instruction_entered[3]][0]
-    ml= opcode+"00"+r1+r2+r3 #converted into machine code 
+    ml= op_code+"00"+r1+r2+r3 #converted into machine code 
     #add function
     if (ml[:5]=="00000"):
-        binary_of_registers[r1][1]=binary_of_registers[r2][1]+binary_of_registers[r3][1]
+        binary_of_registers[r1][1] = binary_of_registers[r2][1]+binary_of_registers[r3][1]
         if binary_of_registers[r1][1]>255: #overflow:when the answer is more than 255
             V=1
             binary_of_registers[r1][1]=0
