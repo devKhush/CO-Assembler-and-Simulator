055df998ae8d9ae8f882102f34a6123e949b0284
devKhush <khushdev20211@iiitd.ac.in>
2021-08-15 10:33:40 +0530

    ...

--- Simple-Assembler/registers.py
+++ Simple-Assembler/registers.py
@@ -12,23 +12,23 @@ def typeA_fun(instruction_entered) :
     op_code = instructions.type_A_instructions[instruction_entered[0]]
 
     #taking register's binary value from the dictionaries
-    r1= binary_of_registers[instruction_entered[1]][0]
-    r2= binary_of_registers[instruction_entered[2]][0]
-    r3= binary_of_registers[instruction_entered[3]][0]
-    ml= op_code+"00"+r1+r2+r3 #converted into machine code 
+    register1_binary= binary_of_registers[instruction_entered[1]][0]
+    register2_binary= binary_of_registers[instruction_entered[2]][0]
+    register3_binary= binary_of_registers[instruction_entered[3]][0]
+    ml= op_code+"00"+register1_binary+register2_binary+register3_binary #converted into machine code 
     #add function
     if (ml[:5]=="00000"):
         binary_of_registers[instruction_entered[1]][1] = binary_of_registers[instruction_entered[2]][1]+binary_of_registers[instruction_entered[3]][1]
-        if binary_of_registers[r1][1]>255: #overflow:when the answer is more than 255
+        if binary_of_registers[register1_binary][1]>255: #overflow:when the answer is more than 255
             V=1
-            binary_of_registers[r1][1]=0      # remove this and set lower 16 bits values into Reg1
+            binary_of_registers[register1_binary][1]=0      # remove this and set lower 16 bits values into Reg1
 
     #subtraction function
     elif (ml[:5]=="00001"):
         binary_of_registers[instruction_entered[1]][1]=binary_of_registers[instruction_entered[2]][1]-binary_of_registers[instruction_entered[3]][1]
-        if binary_of_registers[r1][1]<0:      # overflow : when the sub is less than 0
+        if binary_of_registers[register1_binary][1]<0:      # overflow : when the sub is less than 0
             V=1
-            binary_of_registers[r1][1]=0
+            binary_of_registers[register1_binary][1]=0
 
 
     #multiplication function
@@ -36,7 +36,7 @@ def typeA_fun(instruction_entered) :
         binary_of_registers[instruction_entered[1]][1]=binary_of_registers[instruction_entered[2]][1]*binary_of_registers[instruction_entered[3]][1]
         if binary_of_registers[instruction_entered[1]][1]>255: #overflow:when the answer is more than 255
             V=1
-            binary_of_registers[r1][1]=0  # remove this and set lower 16 bits values into Reg1
+            binary_of_registers[register1_binary][1]=0  # remove this and set lower 16 bits values into Reg1
 
     #bitwise XOR
     elif(ml[:5]=="01010"):
@@ -48,7 +48,7 @@ def typeA_fun(instruction_entered) :
 
     #bitwise AND
     elif (ml[:5]=="01100"):
-        binary_of_registers[r1][1]=binary_of_registers[r2][1] & binary_of_registers[r3][1]
+        binary_of_registers[register1_binary][1]=binary_of_registers[register2_binary][1] & binary_of_registers[register3_binary][1]
         print(ml)
 
 
