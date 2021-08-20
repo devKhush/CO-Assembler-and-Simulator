bc736ff60b63b806ef01844eb25eed3d3a28cfef
devKhush <khushdev20211@iiitd.ac.in>
2021-08-20 21:29:22 +0530

    END Assignment started

--- SimpleSimulator/Simulator_main.py
+++ SimpleSimulator/Simulator_main.py
@@ -1,8 +1,34 @@
+from sys import stdin
+
+class Memory:
+    def __init__(self):
+        self.memoryDict = {}       # {pc : instruction_binary_code}
+
+    def getData(self,pc):
+        instruction = self.memoryDict
+
+
+
+def initialize(memory):
+    address = 0
+    for line in stdin:
+        memory[address] = line
+        address+=1
+
+
+
+
+
+
+
+
+
+memory = Memory()
 initialize(memory) # Load memory from stdin
 PC = 0             # Start from the first instruction
 halted = False
-white(not halted):
-    Instruction = memory.getData(PC)             # Get current instruction
+while not halted:
+    Instruction = memory.getData(PC)          # Get current instruction
     halted, new_PC = EE.execute(Instruction)  # Update RF compute new_PC
     PC.dump()                                 # Print PC
     RF.dump()                                 # Print RF state
